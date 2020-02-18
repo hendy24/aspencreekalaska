39889bac392d510993d60b524ef3363b3f3b3b49
kemish <kemish.hendershot@gmail.com>
2020-02-18 13:01:49 -0900

    Added footer to news page
    Signed-off-by: kemish <kemish.hendershot@gmail.com>

--- news.php
+++ news.php
@@ -5,22 +5,5 @@
 		<iframe src="https://core-repo.com/docs" width="100%" height ="900" style="border: 0"></iframe>
         </div>
     </section>
-    <!-- hero section end here -->
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="vendor/code.jquery.com/jquery-3.4.1.slim.min.js"></script>
-    <script src="vendor/cdn.jsdelivr.net/npm/popper.js%401.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/slick.min.js"></script>
-    <script src="js/scripts.js"></script>
-    <script src="js/main.js"></script>	
-    <script src="vendor/cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.9.0/js/bootstrap-datepicker.min.js"></script>
-    <script type="text/javascript">
-    $('#date').datepicker({});
-    </script>
-	<!-- Start Podium Script -->
-	<script defer src="https://connect.podium.com/widget.js#API_TOKEN=5a2423dd-edc2-41e3-abef-c417d13b723f" id="podium-widget" data-api-token="5a2423dd-edc2-41e3-abef-c417d13b723f"></script>
-	<!-- End Podium Script -->	
-	
-</body>
-</html>
+
+<?php include 'footer.php'; ?>
