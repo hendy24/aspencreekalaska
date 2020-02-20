8f3a1fc1fbce6edcfe099a29407e1e18b8a9d404
kemish <kemish.hendershot@gmail.com>
2020-02-19 17:07:48 -0900

    Added Grav for blog management
    Signed-off-by: kemish <kemish.hendershot@gmail.com>

---
+++ vendor/grav-admin/cache/compiled/files/94141cef58ab8eea68f12c3d12a803c4.yaml.php
@@ -0,0 +1,18 @@
+<?php
+return [
+    '@class' => 'Grav\\Common\\File\\CompiledYamlFile',
+    'filename' => '/Users/kemish/Sites/aspencreekalaska/vendor/grav-admin/system/blueprints/config/streams.yaml',
+    'modified' => 1581466810,
+    'data' => [
+        'title' => 'PLUGIN_ADMIN.FILE_STREAMS',
+        'form' => [
+            'validation' => 'loose',
+            'hidden' => true,
+            'fields' => [
+                'schemes.xxx' => [
+                    'type' => 'array'
+                ]
+            ]
+        ]
+    ]
+];
